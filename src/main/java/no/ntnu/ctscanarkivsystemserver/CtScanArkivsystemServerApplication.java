1027f80e16ca8fb42b935e502a7925190f877b76
Trymv <48278116+Trymv@users.noreply.github.com>
2021-01-28 10:58:25 +0100

    Initial commit

---
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/CtScanArkivsystemServerApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.ctscanarkivsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtScanArkivsystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtScanArkivsystemServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/ntnu/ctscanarkivsystemserver/CtScanArkivsystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.ctscanarkivsystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtScanArkivsystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
