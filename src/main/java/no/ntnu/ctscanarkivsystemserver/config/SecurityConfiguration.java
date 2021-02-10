946a1dd4bfb6fc5db03e452128fe3d2a6bfa26d0
Trym Vaaland <trymv@stud.ntnu.no>
2021-02-10 14:07:02 +0100

    Removed unused imports

--- src/main/java/no/ntnu/ctscanarkivsystemserver/config/SecurityConfiguration.java
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/config/SecurityConfiguration.java
@@ -4,17 +4,13 @@ import no.ntnu.ctscanarkivsystemserver.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import sun.security.util.Password;
 
 /**
  * @author TrymV
