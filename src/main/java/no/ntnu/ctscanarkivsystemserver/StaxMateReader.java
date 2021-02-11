0e79f316b34f28633445a5ccf55fa87cafbe7797
Aleksander Bakken <aleksander-bakken@hotmail.com>
2021-02-11 09:52:39 +0100

    Created documentation for the StaxMateReader class
    Added documentation and some explanation for some of the code. Also cleaned up some test functionality.

--- src/main/java/no/ntnu/ctscanarkivsystemserver/StaxMateReader.java
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/StaxMateReader.java
@@ -12,21 +12,31 @@ import java.util.ArrayList;
 
 /*
 This class is from http://blog.palominolabs.com/2013/03/06/parsing-xml-with-java-and-staxmate/index.html
-Should print a list of
+Should print a list of DicomAttributes' keywords and the DicomAttributes value.
+For now only prints to the log, but will probably in the future use the information gathered for something more useful.
  */
 public class StaxMateReader {
 
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
-        SMInputFactory factory = new SMInputFactory(XMLInputFactory.newFactory());
         String path = "..\\ExampleFiles\\FORMAS\\BMM.CT.THORAX_FORMAS_SOLID3_(ADULT).2.414.2017.11.08.12.26.03.232500.27001744.xml";
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("test");
+        SMInputFactory factory = new SMInputFactory(XMLInputFactory.newFactory());
+        ArrayList<String> strings;
         strings = parse(path, factory);
         for (int i = 0; i < strings.size(); i++) {
             System.out.println("List item " + i + ": " + strings.get(i));
         }
     }
 
+    /**
+     * Creates a root cursor and starts a while loop that continues as long as there are
+     * elements to iterate over.
+     *
+     * @param path the path to the file you want to read from.
+     * @param factory a StaxMate input factory. Is used to create a cursor which iterates through the xml file.
+     * @return stringList a list which should contain DicomAttributes' keywords and their values.
+     * @throws XMLStreamException If something goes wrong with the iterating over the document.
+     * @throws FileNotFoundException If the file from path is not found.
+     */
     public static ArrayList<String> parse(String path, SMInputFactory factory) throws XMLStreamException, FileNotFoundException {
         ArrayList<String> stringList = new ArrayList<>();
 
@@ -46,14 +56,24 @@ public class StaxMateReader {
         
         return stringList;
     }
-    /*
-    rootChildCursor points at the <dicomAttribute> element
-    rootChildCursor.childElementCursor() points at <Value> elements
-    */
+
+    /**
+     * rootChildCursor points at the <dicomAttribute> element
+     * rootChildCursor.childElementCursor() points at <Value> elements
+     * Will check if the cursor is pointing at a valid element (In this case, <DicomAttribute> or <Item>)
+     * If the cursor is pointing at a valid element it will go into the corresponding case in the switch
+     * and get the information we want.
+     *
+     * @param stringList A list of the information we want saved as strings.
+     * @param rootChildCursor A cursor that points at the DicomAttribute elements.
+     * @throws XMLStreamException If something goes wrong with the iterating over the document.
+     */
     private static void handleRootChildElement(ArrayList<String> stringList, SMInputCursor rootChildCursor) throws XMLStreamException {
         switch (rootChildCursor.getLocalName()) {
             case "DicomAttribute":
+                //Gets the attribute value of the attribute keyword in a DicomAttribute element.
                 stringList.add(rootChildCursor.getAttrValue("keyword"));
+                //Gets the element value of the child elements (usually <Value>)
                 handleValueElement(stringList, rootChildCursor.childElementCursor());
                 break;
                 //TODO: Figure out if this needs implementing
@@ -65,6 +85,14 @@ public class StaxMateReader {
         }
     }
 
+    /**
+     * Loops as long as there is a child element of the parent cursor. Gets the element string value
+     * and adds it to the list of information.
+     *
+     * @param stringList List which will hold the information we want.
+     * @param valueCursor Cursor which will iterate over the child elements of a DicomAttribute. This means mainly <Value>.
+     * @throws XMLStreamException If something goes wrong with the iterating over the document.
+     */
     private static void handleValueElement(ArrayList<String> stringList, SMInputCursor valueCursor) throws XMLStreamException {
         while (valueCursor.getNext() != null) {
             String v = valueCursor.getElemStringValue();
