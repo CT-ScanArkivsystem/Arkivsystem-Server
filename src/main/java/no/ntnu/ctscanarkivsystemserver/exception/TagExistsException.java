67448084ea1a4fc8b3e1a49e7b11779df234c5a8
Trym Vaaland <trymv@stud.ntnu.no>
2021-02-25 16:25:58 +0100

    Added TagExistsException

---
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/exception/TagExistsException.java
@@ -0,0 +1,7 @@
+package no.ntnu.ctscanarkivsystemserver.exception;
+
+public class TagExistsException extends RuntimeException {
+    public TagExistsException(String tagName) {
+        super("A tag with the name: " + tagName + " already exist in the system!");
+    }
+}
