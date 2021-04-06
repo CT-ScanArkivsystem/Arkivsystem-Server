fcac09f21a5d9a4fa5c76a6ac642ab9c853811be
Trym Vaaland <trymv@stud.ntnu.no>
2021-04-06 16:48:55 +0200

    Fixed File class

--- src/main/java/no/ntnu/ctscanarkivsystemserver/model/File.java
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/model/File.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity(name = "files")
 @NamedQuery(name = File.FIND_FILE_BY_NAME_AND_PROJECT, query =
-        "SELECT f FROM files f WHERE f.fileName LIKE: fileName AND f.inProject =: projectId")
+        "SELECT f FROM files f WHERE f.fileName LIKE: fileName AND f.inProject.projectId =: projectId AND f.subFolder LIKE: subFolder")
 public class File {
     public static final String FIND_FILE_BY_NAME_AND_PROJECT = "File.findFileByNameAndProject";
 
@@ -27,6 +27,9 @@ public class File {
     @Column(name="file_name")
     private String fileName;
 
+    @Column(name="sub_folder")
+    private String subFolder;
+
     @ManyToOne
     @JoinColumn(
             name="in_project",
@@ -34,7 +37,7 @@ public class File {
     )
     private Project inProject;
 
-    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinTable(name = "file_tags",
             joinColumns = @JoinColumn(
                     name = "file_id",
@@ -49,9 +52,10 @@ public class File {
      * @param fileName
      * @param project
      */
-    public File(String fileName, Project project) {
+    public File(String fileName, String subFolder, Project project) {
         this.fileId = UUID.randomUUID();
         this.fileName = fileName;
+        this.subFolder = subFolder;
         this.inProject = project;
     }
 }
