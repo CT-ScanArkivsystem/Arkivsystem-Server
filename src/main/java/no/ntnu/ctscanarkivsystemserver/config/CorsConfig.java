988540957556a2ae1997e9f1b07ffd50f1fdd090
Trym Vaaland <trymv@stud.ntnu.no>
2021-04-15 12:04:59 +0200

    Fixed CorsConfig to get domain from application.properties

--- src/main/java/no/ntnu/ctscanarkivsystemserver/config/CorsConfig.java
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package no.ntnu.ctscanarkivsystemserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //This class was taken from this guide: https://www.youtube.com/watch?v=PovQ6aUeRgg
 @Configuration
 public class CorsConfig {
+
+    private final String domain;
+    private final String port;
+
+    @Autowired
+    public CorsConfig(Properties properties) {
+        this.domain = properties.getDomain();
+        this.port = properties.getPort();
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -19,7 +29,7 @@ public class CorsConfig {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         // TODO: When the connection becomes secure (HTTPS), change the IP to include an s!
-                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                        .allowedOrigins("http://" + domain + ":" + port, "http://127.0.0.1:3000")
                         .exposedHeaders("Set-Cookie")
                         .allowCredentials(true);
             }
