d75a9d188b6ddb959b2e7c7a8bdae1d58d404b53
Trym Vaaland <trymv@stud.ntnu.no>
2021-04-16 16:22:07 +0200

    Fixed function to schedule date/time of restart

--- src/main/java/no/ntnu/ctscanarkivsystemserver/service/ServerService.java
+++ src/main/java/no/ntnu/ctscanarkivsystemserver/service/ServerService.java
@@ -1,27 +1,35 @@
 package no.ntnu.ctscanarkivsystemserver.service;
 
+import no.ntnu.ctscanarkivsystemserver.CtScanArkivsystemServerApplication;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class ServerService {
 
-    public boolean scheduleServerRestart(Date timeOfRestart) throws ParseException {
-        System.out.println("Restarting!");
-        System.out.println(getTimeUntilRestart(timeOfRestart));
-        return true;
+    /**
+     * Schedule a server restart at the date and time of parameter.
+     * @param timeOfRestart date and time of restart formatted as: YYYY-MM-ddThh:mm+0200
+     */
+    public void scheduleServerRestart(Date timeOfRestart) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        long timeUntilRestart = getTimeUntilRestart(timeOfRestart);
+        executorService.schedule(CtScanArkivsystemServerApplication::restart, timeUntilRestart, TimeUnit.MINUTES);
+        System.out.println("Server is restarting in ~" + timeUntilRestart + " minutes.");
     }
 
+    /**
+     * Gets the time of restart in minutes.
+     * @param timeOfRestart date and time of restart.
+     * @return time until restart in minutes.
+     */
     private long getTimeUntilRestart(Date timeOfRestart) {
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Oslo"));
 
